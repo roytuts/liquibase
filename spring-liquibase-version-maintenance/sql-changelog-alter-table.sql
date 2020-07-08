fec705823fd246493fc8fb7659ffa22be12e4349
Soumitra <projwebinfo@gmail.com>
2020-07-08 14:52:36 +0530

    Add files via upload

---
+++ spring-liquibase-version-maintenance/sql-changelog-alter-table.sql
@@ -0,0 +1,76 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.17 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for roytuts
+CREATE DATABASE IF NOT EXISTS `roytuts` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `roytuts`;
+
+-- Dumping structure for table roytuts.category
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `sort_order` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `unique` (`category_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table roytuts.category: ~0 rows (approximately)
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+
+-- Dumping structure for table roytuts.databasechangelog
+CREATE TABLE IF NOT EXISTS `databasechangelog` (
+  `ID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `AUTHOR` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `FILENAME` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DATEEXECUTED` datetime NOT NULL,
+  `ORDEREXECUTED` int(11) NOT NULL,
+  `EXECTYPE` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MD5SUM` varchar(35) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `DESCRIPTION` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `COMMENTS` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `TAG` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LIQUIBASE` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `CONTEXTS` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `LABELS` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `DEPLOYMENT_ID` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table roytuts.databasechangelog: ~1 rows (approximately)
+/*!40000 ALTER TABLE `databasechangelog` DISABLE KEYS */;
+INSERT INTO `databasechangelog` (`ID`, `AUTHOR`, `FILENAME`, `DATEEXECUTED`, `ORDEREXECUTED`, `EXECTYPE`, `MD5SUM`, `DESCRIPTION`, `COMMENTS`, `TAG`, `LIQUIBASE`, `CONTEXTS`, `LABELS`, `DEPLOYMENT_ID`) VALUES
+	('create_table', 'Soumitra Roy', 'classpath:liquibase/db.changelog-master.yml', '2020-07-07 17:23:38', 1, 'EXECUTED', '8:59850f7fc0106f6a915f9921f69370e5', 'sqlFile', '', NULL, '3.10.1', NULL, NULL, '4122818025'),
+	('alter_table', 'Soumitra Roy', 'classpath:liquibase/db.changelog-master.yml', '2020-07-07 17:38:40', 2, 'EXECUTED', '8:831f648d261a82f879344307d427f20a', 'sqlFile', '', NULL, '3.10.1', NULL, NULL, '4123720100');
+/*!40000 ALTER TABLE `databasechangelog` ENABLE KEYS */;
+
+-- Dumping structure for table roytuts.databasechangeloglock
+CREATE TABLE IF NOT EXISTS `databasechangeloglock` (
+  `ID` int(11) NOT NULL,
+  `LOCKED` bit(1) NOT NULL,
+  `LOCKGRANTED` datetime DEFAULT NULL,
+  `LOCKEDBY` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table roytuts.databasechangeloglock: ~1 rows (approximately)
+/*!40000 ALTER TABLE `databasechangeloglock` DISABLE KEYS */;
+INSERT INTO `databasechangeloglock` (`ID`, `LOCKED`, `LOCKGRANTED`, `LOCKEDBY`) VALUES
+	(1, b'0', NULL, NULL);
+/*!40000 ALTER TABLE `databasechangeloglock` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
